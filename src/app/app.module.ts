34524e6d823e1c280f7ef5115248e7ce9917a967
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-20 15:32:24 -0300

    creado componente acercade ng g compoment components/acercade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
