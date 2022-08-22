25877148654bcfc8f02af39d8f9c579ab352035a
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-22 18:07:20 -0300

    creado componente CIRCLE-PROGRESS-BAR

---
+++ src/app/components/circle-progress-bar/circle-progress-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-circle-progress-bar',
+  templateUrl: './circle-progress-bar.component.html',
+  styleUrls: ['./circle-progress-bar.component.css']
+})
+export class CircleProgressBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
