4db865f761d9b551acad98bbcaa06ac909263984
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-29 11:17:19 -0300

    interceptor completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -35,7 +36,7 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule
   ],
-  providers: [],
+  providers: [interceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/service/interceptor-service.ts
+++ src/app/service/interceptor-service.ts
@@ -1,7 +1,11 @@
 import { HttpEvent, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { TokenService } from "./token.service";
 
+@Injectable({
+  providedIn: 'root'
+})
 export class InterceptorService {
   constructor(private tokenService: TokenService){}
 
