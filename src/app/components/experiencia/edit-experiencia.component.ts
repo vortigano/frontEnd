aa254aa5cf401448ce54b050700146fb4756b0cd
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-29 19:59:03 -0300

    add edit-experiencia component completo

--- src/app/components/experiencia/edit-experiencia.component.ts
+++ src/app/components/experiencia/edit-experiencia.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Experiencia } from 'src/app/model/experiencia';
+import { SExperienciaService } from 'src/app/service/s-experiencia.service';
 
 @Component({
   selector: 'app-edit-experiencia',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-experiencia.component.css']
 })
 export class EditExperienciaComponent implements OnInit {
-
-  constructor() { }
+  expLab: Experiencia = null;
+  
+  constructor(private sExperiencia: SExperienciaService, private activatedRouter: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    //obtiene el id de la experiencia que vamos a modificar
+    const id= this.activatedRouter.snapshot.params['id'];
+    console.log('modificando: ' + id);
+    this.sExperiencia.detail(id).subscribe(
+      data =>{
+        this.expLab = data;
+      }, err=>{
+        alert("Error al modificar experiencia");
+        this.router.navigate(['']);
+      }
+    )
   }
 
+  onUpdate(): void {
+    const id= this.activatedRouter.snapshot.params['id'];
+    this.sExperiencia.update(id, this.expLab).subscribe(
+      data => {
+        this.router.navigate(['']);
+      }, err =>{
+        alert("Error al modificar experiencia");
+        this.router.navigate(['']);
+      }
+    )
+  }
 }
