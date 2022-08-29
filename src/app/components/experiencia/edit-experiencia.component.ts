7bb4c67f64f02345248b6d81240b936806ed3ef8
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-29 17:54:03 -0300

    creado component edit-experiencia

---
+++ src/app/components/experiencia/edit-experiencia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-experiencia',
+  templateUrl: './edit-experiencia.component.html',
+  styleUrls: ['./edit-experiencia.component.css']
+})
+export class EditExperienciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
