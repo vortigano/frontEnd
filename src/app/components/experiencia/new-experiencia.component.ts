892ab45737c57cacd8304fc2cdc81cfb3cb136c1
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-29 15:21:21 -0300

    creado compomente new-experiencia

---
+++ src/app/components/experiencia/new-experiencia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-experiencia',
+  templateUrl: './new-experiencia.component.html',
+  styleUrls: ['./new-experiencia.component.css']
+})
+export class NewExperienciaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
