631f8f6db7ca1763e4debf50126a6a45ffbfd79a
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-29 17:48:21 -0300

    creado component new-experiencia

--- src/app/components/experiencia/new-experiencia.component.ts
+++ src/app/components/experiencia/new-experiencia.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Experiencia } from 'src/app/model/experiencia';
+import { SExperienciaService } from 'src/app/service/s-experiencia.service';
 
 @Component({
   selector: 'app-new-experiencia',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-experiencia.component.css']
 })
 export class NewExperienciaComponent implements OnInit {
-
-  constructor() { }
+  nombreE:      string = '';
+  descripcionE: string = '';
+  constructor(private sExperiencia: SExperienciaService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onCreate(): void {
+    const expe = new Experiencia(this.nombreE, this.descripcionE);
+    this.sExperiencia.save(expe).subscribe(
+      data=>{
+        alert("Experiencia añadida");
+        this.router.navigate(['']);
+    }, err =>{
+      alert("Falló");
+      this.router.navigate(['']);
+    }
+    )
   }
 
 }
