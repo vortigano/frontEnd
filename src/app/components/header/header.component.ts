c8c186d204a2a3b61a563fc038c4508e3e60ee43
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-08-29 10:52:37 -0300

    botón login cambia según estado de la sesion

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isLogged = false;
 
-  constructor() { }
+  constructor(private router: Router, private tokenService: TokenService) { }
 
   ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    }else{
+      this.isLogged = false;
+    }
   }
 
+  /**
+   * Borra las credenciales y vuelve a recargar la página
+   */
+  onLogout(): void{
+    this.tokenService.logOut();
+    window.location.reload();
+  }
+
+  logIn(){
+    this.router.navigate(['/login'])
+  }
 }
