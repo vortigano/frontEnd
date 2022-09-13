5e4c1841c20f248ec123bbea592aadcaf2b44090
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-09-13 18:41:19 -0300

    update component edit-skill

--- src/app/components/hard-and-soft-skills/edit-skill.component.ts
+++ src/app/components/hard-and-soft-skills/edit-skill.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Skill } from 'src/app/model/skill';
+import { SkillService } from 'src/app/service/skill.service';
 
 @Component({
   selector: 'app-edit-skill',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-skill.component.css']
 })
 export class EditSkillComponent implements OnInit {
+  skill: Skill = null;
 
-  constructor() { }
+  constructor(private skillS: SkillService, private activateRouter: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    const id = this.activateRouter.snapshot.params['id'];
+    this.skillS.detail(id).subscribe(
+      data=>{
+        this.skill = data;
+      }, err=>{
+        alert("Error al modificar habilidad");
+        this.router.navigate(['']);
+      }
+    )
   }
 
+  onUpdate(){
+    const id = this.activateRouter.snapshot.params['id'];
+    this.skillS.update(id, this.skill).subscribe(
+      data=>{
+        this.router.navigate(['']);
+      }, err=>{
+        alert("Error al modificar la habilidad");
+        this.router.navigate(['']);
+      }
+    )
+  }
 }
