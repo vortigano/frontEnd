ddc23cd33fdf759f7369dfce68671bec02941cff
Alberto Martín Capurro <44713214+vortigano@users.noreply.github.com>
2022-10-15 03:48:57 -0300

    add persona service

---
+++ src/app/service/s-persona.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { persona } from '../model/persona';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonaService {
+  //https://backendcam.herokuapp.com/
+  //baseURL = 'http://localhost:8080/personas/';
+  //baseURL = 'https://backendcam.herokuapp.com/personas/';
+  baseURL = environment.urlBase + 'personas/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getPersona(): Observable<persona>{
+    return this.httpClient.get<persona>(this.baseURL + 'traer/perfil');
+  }
+
+  public update(id: number, persona: persona): Observable<any>{
+    return this.httpClient.put<any>(this.baseURL + `editar/${id}`, persona);
+  }
+}
